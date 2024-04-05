08003c6cd0bf188ee9d7e6aea899267f6eb3e545
emilyychau <emilychau.yvo@gmail.com>
2024-04-05 04:03:41 -0400

    update footer

--- wp-content/themes/ux221-l07/customElements.js
+++ wp-content/themes/ux221-l07/customElements.js
@@ -9,8 +9,18 @@ customElements.define("x-year", CopyrightYear);
 //
 class TwoSidedMarket extends HTMLElement {
 	connectedCallback(){
-		this.innerHTML = `<a href="side-a">Students</a>&nbsp;<a href="side-b">Employers</a>`;
-	}
+		this.innerHTML = `<a href="side-a" class="item">Students</a>&ensp;&ensp;<a href="side-b" class="item">Employers</a>`;
+		this.style.display = 'flex';
+        this.style.justifyContent = 'center';
+		const items = this.querySelectorAll('.item');
+        items.forEach(item => {
+            item.style.border = '2px solid black';
+            item.style.margin = '2px';
+            item.style.padding = '4px';
+			item.style.borderRadius = '5px';
+            item.style.display = 'inline-block'; // alignment
+        });
+    }
 }
 
 customElements.define("x-twosided", TwoSidedMarket);

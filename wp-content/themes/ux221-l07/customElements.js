330a70e7b625c0025db96faadcc4df9cd90088a8
emilyychau <emilychau.yvo@gmail.com>
2024-04-05 01:54:10 -0400

    add custom element

--- wp-content/themes/ux221-l07/customElements.js
+++ wp-content/themes/ux221-l07/customElements.js
@@ -5,3 +5,12 @@ class CopyrightYear extends HTMLElement{
 }
 
 customElements.define("x-year", CopyrightYear);
+
+//
+class TwoSidedMarket extends HTMLElement {
+	connectedCallback(){
+		this.innerHTML = `<a href="side-a">Students</a>&nbsp;<a href="side-b">Employers</a>`;
+	}
+}
+
+customElements.define("x-twosided", TwoSidedMarket);
